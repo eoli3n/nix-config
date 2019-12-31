6893dc9ae00b69a58e0b5a37123111f44f821e55
eoli3n <jonathan.kirszling@umontpellier.fr>
2019-12-31 16:49:31 +0100

    sway config updated

--- modules/sway.nix
+++ modules/sway.nix
@@ -68,12 +68,6 @@
     };
   };
 
-  services.redshift = {
-    enable = true;
-    # Redshift with wayland support isn't present in nixos-19.09 atm. You have to cherry-pick the commit from https://github.com/NixOS/nixpkgs/pull/68285 to do that.
-    package = pkgs.redshift-wlr;
-  };
-
   programs.waybar.enable = true;
 
   systemd.user.services.kanshi = {
