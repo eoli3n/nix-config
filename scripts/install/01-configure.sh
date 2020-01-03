bdb5ff3cbd844ce71213797d549e1d14093dce1b
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-01-03 12:22:27 +0100

    print bold

--- scripts/install/01-configure.sh
+++ scripts/install/01-configure.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+print () {
+    echo "\033[1m>$1"
+}
+
 # Set DISK
 select ENTRY in $(ls /dev/disk/by-id/);
 do
@@ -10,7 +14,7 @@ do
     break
 done
 
-read -p "Do you want to swipe all datas on $ENTRY ?" -n 1 -r
+read -p "> Do you want to swipe all datas on $ENTRY ?" -n 1 -r
 echo # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
@@ -19,9 +23,11 @@ then
 fi
 
 # EFI part
+print "Creating EFI part"
 sgdisk -n1:1M:+512M -t1:EF00 $DISK
 
 # LUKS part
+print "Creating LUKS part"
 sgdisk -n2:0:0 -t2:8309 $DISK
 
 # Inform kernel
@@ -29,27 +35,31 @@ partprobe $DISK
 
 # Format boot part
 sleep 1
+print "Format EFI part"
 mkfs.vfat $DISK-part1
 
 # Luks root
-echo "Configure LUKS..."
+print "Create LUKS"
 cryptsetup luksFormat $DISK-part2
+print "Open LUKS"
 cryptsetup luksOpen $DISK-part2 luksroot
 LUKS="/dev/mapper/luksroot"
 
 # Create ZFS pool
+print "Create ZFS pool"
 zpool create -O mountpoint=none -R /mnt rpool $LUKS
 
 # ZFS filesystems
+print "Create ZFS volumes"
 zfs create -o mountpoint=none rpool/root
 zfs create -o mountpoint=legacy rpool/root/nixos
 zfs create -o mountpoint=legacy rpool/home
 
 # Mount filesystems
+print "Mount parts"
 mount -t zfs rpool/root/nixos /mnt
 mkdir /mnt/home
 mount -t zfs rpool/home /mnt/home
-
 mkdir /mnt/boot
 mount $DISK-part1 /mnt/boot
 
