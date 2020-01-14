dcc2327847fa68ee9b412820b46697020b4d24a6
eoli3n <jkirsz@gmail.com>
2020-01-14 09:28:11 +0100

    update config iwd

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
   # Enable wifi
-  networking.wireless.enable = true;
+  #networking.wireless.enable = true;
   networking.wireless.iwd.enable = true;
 }
