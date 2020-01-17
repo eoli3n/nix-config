56169337c4251d3d2a74317d0bfab1a2c2c77941
eoli3n <jkirsz@gmail.com>
2020-01-17 14:52:02 +0100

    added work config

---
+++ hosts/work/configuration.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  # Do not change
+  system.stateVersion = "19.09";
+
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../modules/default.nix
+      ../../modules/desktop.nix
+      #../../modules/laptop.nix
+    ];
+
+  # Intel microcode
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "work";
+  networking.useDHCP = false;
+
+  # ZFS
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "2020abab";
+  services.zfs.autoScrub.enable = true;
+  boot.zfs.requestEncryptionCredentials = true;
+}
+
